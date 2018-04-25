11f3629c64ffc452ab28997e7fea2b999106c560
Keenhazard <khinhazzard@gmail.com>
2018-04-24 22:28:07 -0700

    1.01

---
+++ Exercises/src/GFG_default.java
@@ -0,0 +1,19 @@
+//for copypaste
+import java.util.Scanner;
+
+public class GFG_default {
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Input:");
+
+		int tc = sc.nextInt();
+
+		while (tc-- > 0) {
+
+			// adds newline between testcases
+			if (tc > 0)
+				System.out.println();
+		}
+	}
+}
